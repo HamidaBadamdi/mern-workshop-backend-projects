4a526d3c1e3858c918d9277bd274aeb3028a2a75
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2026-06-05 18:03:16 +0530

    Day 2: MongoDB integration, API creation & Postman testing
    Day 2 of MERN Stack Workshop 🚀
    
    * Explored MongoDB using MongoDB Compass and MongoDB Shell
    * Created database and collections, and inserted data using commands
    * Learned and implemented route parameters and query strings in Express.js
    * Used middleware (express.json()) for handling JSON data
    * Built REST APIs for sending and retrieving data
    * Tested APIs using Postman (GET & POST methods)
    * Successfully passed and received data in JSON format
    
    This session focused on integrating backend logic with database operations and understanding real-world API workflows.

--- ExpressJS_Project/app.js
+++ ExpressJS_Project/app.js
@@ -1,17 +1,59 @@
 const express = require('express');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mudb2')
+.then(() => console.log('Connected to MongoDB'))
+.catch(() => console.log('Failed to connect to MongoDB'));
+
+var ProductModel = require('./models/Product')
+
 const app = express();
 const port = 4000;
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/savedata' , (req, res) => {
+
+  var userdata = {
+    pname : 'Vivo Y22',
+    pprice : 15000,
+    pdetails : 'Latest smartphone with great features'
+  }
+
+   var myData = ProductModel(userdata)
+    myData.save()
+    .then(() => res.send('Data saved successfully'))
+
+});
+
+app.post('/addData' , (req , res) => {
+
+  ProductModel.create(req.body)
+  .then(() =>{
+    res.json({flag:1 , msg:'Data added successfully'})
+  })
+
+})
+
+app.get(('/getdata') , (req,res) =>{
+  ProductModel.find()
+  .then((data) => 
+    res.send(data))
+  
+})
+
+
 app.get('/home', (req, res) => {
  // res.send('Home Page!');
  res.sendFile(__dirname + '/home.html');
 });
+
 
 app.get('/about', (req, res) => {
   //res.send('About Page!');
